15e1e9e4d17fe200551ecb8b9617e23f4d6a0e0d
Guillaume Perrin <gperrin01@gmail.com>
2015-05-21 16:25:33 +0100

    bike can be fixed

--- lib/bike.rb
+++ lib/bike.rb
@@ -8,9 +8,13 @@ class Bike
   def broken?
     @broken 
   end
-
   def break
+    # can you break on a bike already broken?
     @broken = true
+  end
+  def fix
+    #can you fix on a non-broken bike?
+    @broken = false
   end
 
 
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -11,5 +11,11 @@ describe Bike do
     bike.break
     expect(bike.broken?).to be true
   end
+  it 'should be able to get fixed' do
+    bike = Bike.new
+    bike.break
+    bike.fix
+    expect(bike.broken?).to be false
+  end
 
 end
